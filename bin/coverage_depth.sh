87c6d9477ccf733a258146a5c38dc2d1b2eb839b
bbaggins2005 <bbaggins2005@gmail.com>
2025-03-16 22:02:11 -0500

    Created coverage_depth.sh

---
+++ bin/coverage_depth.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# name:		coverage_depth.sh
+# author: 	Reece Chae
+# description:	Basic wrapper script for coverage and depth analysis
+# https://medium.com/ngs-sh/coverage-analysis-from-the-command-line-542ef3545e2c
+# Requires image: staphb/bedtools
+
+INPUT=$1
+OUTPUTFILEPREFIX=$(basename ${INPUT})
+OUTPUTDIR="data"
+LOGDIR="log"
+
+[ -d "${OUTPUTDIR}" ] || mkdir -p ${OUTPUTDIR}
+[ -d "${LOGDIR}" ] || mkdir -p ${LOGDIR}
+OUTPUTFILE="${OUTPUTDIR}/${OUTPUTFILEPREFIX}.txt"
+LOGFILE="${LOGDIR}/coverage_depth_${OUTPUTFILEPREFIX}.log"
+echo "info: Starting bedtools for ${INPUT}" | tee -a ${LOGFILE}
+bedtools genomecov -ibam ${INPUT} -bga | grep -w "0$" > ${OUTPUTFILE}
+echo "info: Completed. See ${OUTPUTFILE}" | tee -a ${LOGFILE}
