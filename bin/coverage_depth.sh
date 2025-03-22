c8ff8d6c7068aee2e6e958b2066b4896f0d9127c
bbaggins2005 <bbaggins2005@gmail.com>
2025-03-21 19:45:10 -0500

    Updated coverage_depth.sh

--- bin/coverage_depth.sh
+++ bin/coverage_depth.sh
@@ -6,14 +6,24 @@
 # Requires image: staphb/bedtools
 
 INPUT=$1
+MODE=$2
 OUTPUTFILEPREFIX=$(basename ${INPUT})
 OUTPUTDIR="data"
 LOGDIR="log"
 
 [ -d "${OUTPUTDIR}" ] || mkdir -p ${OUTPUTDIR}
 [ -d "${LOGDIR}" ] || mkdir -p ${LOGDIR}
-OUTPUTFILE="${OUTPUTDIR}/${OUTPUTFILEPREFIX}.bed"
+OUTPUTFILE="${OUTPUTDIR}/${OUTPUTFILEPREFIX}"
 LOGFILE="${LOGDIR}/coverage_depth_${OUTPUTFILEPREFIX}.log"
 echo "info: Starting bedtools for ${INPUT}" | tee -a ${LOGFILE}
-bedtools genomecov -ibam ${INPUT} -bga | grep -w "0$" > ${OUTPUTFILE}
-echo "info: Completed. See ${OUTPUTFILE}" | tee -a ${LOGFILE}
+if [ "${MODE,,}" = "zero" ]; then
+    echo "info: generating bed file for zero coverage"
+    bedtools genomecov -ibam ${INPUT} -bga | grep -w "0$" > ${OUTPUTFILE}_zero.bed
+elif [ "${MODE,,}" = "all" ]; then
+    echo "info: generating bed file for all coverage"
+    bedtools genomecov -ibam ${INPUT} -bga > ${OUTPUTFILE}_all.bed
+else
+    echo "info: generating bed file for standard coverage"
+    bedtools genomecov -ibam ${INPUT} -bg > ${OUTPUTFILE}.bed
+fi
+echo "info: Completed. See ${OUTPUTFILE} bed file" | tee -a ${LOGFILE}
