c7982e904c61eea13147e1460092a0c018cce77d
bbaggins2005 <bbaggins2005@gmail.com>
2025-04-03 21:04:04 -0500

    Updated coverage_depth_pct_plot.py

--- bin/coverage_depth_pct_plot.py
+++ bin/coverage_depth_pct_plot.py
@@ -1,27 +1,29 @@
 #!/usr/bin/env python
 #
-# Name: coverage_depth_pctplot.py
+# Name: coverage_depth_pct_plot.py
 # Author: Reece Chae
 # Description:
 
 import matplotlib.pyplot as plt
 import dask.dataframe as dd
+import os
 import re
 import glob
 from concurrent.futures import ThreadPoolExecutor
 
 chromosome = "chr1"
+parquet_dir = "tmp"
+os.makedirs(parquet_dir, exist_ok=True)
 
 def process_bed_file(file):
     df = dd.read_csv(file, sep="\t", header=None, names=["chrom", "start", "end", "depth"])
     df_chrom = df[df["chrom"] == chromosome ]
     df_chrom["name"] = re.sub(r"\.bed$", "", file, flags=re.IGNORECASE)
-    return df_chrom
+    parquet_bed_file_dir = os.path.join(parquet_dir, f"{os.path.basename(file)}.parquet")
+    df_chrom.to_parquet(parquet_bed_file_dir, engine="pyarrow", write_index=False)
 
 bed_files = glob.glob("*.bed")
 with ThreadPoolExecutor(max_workers=8) as executor:
-    results = executor.map(process_bed_file, bed_files)
-
-for df_chrom in results:
-    for name, chrom, start, end, depth in zip(df_chrom["name"], df_chrom["chrom"], df_chrom["start"], df_chrom["end"], df_chrom["depth"]):
-        print(name, chrom, start, end, depth) 
+    executor.map(process_bed_file, bed_files)
+df_parquet_data = dd.read_parquet(parquet_dir + '/**/*.parquet', engine="pyarrow", recursive=True)
+print(df_parquet_data.compute())
